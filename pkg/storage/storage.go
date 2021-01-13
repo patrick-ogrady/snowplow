0b32f92c1fd2ce545cddbf80c35034ea097a8963
Patrick O'Grady <prohb125@gmail.com>
2021-01-13 08:34:36 -0800

    don't load entire download into memory

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -43,7 +43,7 @@ func Upload(ctx context.Context, bucket string, name string) error {
 	// Open local file.
 	f, err := os.Open(name)
 	if err != nil {
-		return fmt.Errorf("%w: could not open file", err)
+		return fmt.Errorf("%w: could not open file %s", err, name)
 	}
 	defer f.Close()
 
@@ -98,9 +98,8 @@ func upload(ctx context.Context, bucket string, name string, blob io.Reader) err
 // Download retrieves a file from a bucket with a
 // given name.
 func Download(ctx context.Context, bucket string, name string) error {
-	data, err := download(ctx, bucket, name)
-	if err != nil {
-		return fmt.Errorf("%w: unable to download name", err)
+	if err := download(ctx, bucket, name); err != nil {
+		return fmt.Errorf("%w: unable to download %s", err, name)
 	}
 
 	dChecksum, err := downloadString(
@@ -112,7 +111,13 @@ func Download(ctx context.Context, bucket string, name string) error {
 		return fmt.Errorf("%w: unable to download checksum", err)
 	}
 
-	checksum, err := integrity.Checksum(bytes.NewReader(data))
+	f, err := os.Open(name)
+	if err != nil {
+		return fmt.Errorf("%w: could not open file %s", err, name)
+	}
+	defer f.Close()
+
+	checksum, err := integrity.Checksum(f)
 	if err != nil {
 		return fmt.Errorf("%w: could not get checksum of credentials", err)
 	}
@@ -121,27 +126,14 @@ func Download(ctx context.Context, bucket string, name string) error {
 		return fmt.Errorf("expected checksum %s but got %s", dChecksum, checksum)
 	}
 
-	if err := ioutil.WriteFile(name, data, 0644); err != nil {
-		return fmt.Errorf("%w: unable to write %s to disk", err, name)
-	}
-
 	return nil
 }
 
 // downloadString downloads a string from name.
 func downloadString(ctx context.Context, bucket string, name string) (string, error) {
-	data, err := download(ctx, bucket, name)
-	if err != nil {
-		return "", fmt.Errorf("%w: unable to download name", err)
-	}
-
-	return string(data), nil
-}
-
-func download(ctx context.Context, bucket string, name string) ([]byte, error) {
 	client, err := storage.NewClient(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("%w: could not create new storage client", err)
+		return "", fmt.Errorf("%w: could not create new storage client", err)
 	}
 	defer client.Close()
 
@@ -150,14 +142,43 @@ func download(ctx context.Context, bucket string, name string) ([]byte, error) {
 
 	rc, err := client.Bucket(bucket).Object(name).NewReader(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("Object(%q).NewReader: %v", name, err)
+		return "", fmt.Errorf("Object(%q).NewReader: %v", name, err)
 	}
 	defer rc.Close()
 
 	data, err := ioutil.ReadAll(rc)
 	if err != nil {
-		return nil, fmt.Errorf("ioutil.ReadAll: %v", err)
+		return "", fmt.Errorf("ioutil.ReadAll: %v", err)
 	}
 
-	return data, nil
+	return string(data), nil
+}
+
+func download(ctx context.Context, bucket string, name string) error {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("%w: could not create new storage client", err)
+	}
+	defer client.Close()
+
+	ctx, cancel := context.WithTimeout(ctx, defaultTimeout)
+	defer cancel()
+
+	rc, err := client.Bucket(bucket).Object(name).NewReader(ctx)
+	if err != nil {
+		return fmt.Errorf("Object(%q).NewReader: %v", name, err)
+	}
+	defer rc.Close()
+
+	w, err := os.Create(name)
+	if err != nil {
+		return fmt.Errorf("%w: unable to create file %s", err, name)
+	}
+	defer w.Close()
+
+	if _, err := io.Copy(w, rc); err != nil {
+		return fmt.Errorf("%w: unable to download to %s", err, name)
+	}
+
+	return nil
 }
