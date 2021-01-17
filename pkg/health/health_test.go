5d98b8fc135b7e595e47b155d6852e9995c32c2e
Patrick O'Grady <prohb125@gmail.com>
2021-01-17 13:19:03 -0800

    pass tests

--- pkg/health/health_test.go
+++ pkg/health/health_test.go
@@ -60,16 +60,24 @@ func handleIsHealthyChecks(n *mocks.Notifier, c *mocks.Client) {
 	c.On("IsHealthy").Return(false, nil).Once()
 }
 
-func handlePeers(n *mocks.Notifier, c *mocks.Client) {
+func handlePeers(n *mocks.Notifier, c *mocks.Client, mw *mocks.MetricWriter) {
 	c.On("Peers").Return(uint64(0), nil).Once()
+	mw.On("Peers", mock.Anything, uint64(0)).Return(nil).Once()
 	c.On("Peers").Return(uint64(2), nil).Once()
+	mw.On("Peers", mock.Anything, uint64(2)).Return(nil).Once()
 	c.On("Peers").Return(uint64(3), nil).Once()
+	mw.On("Peers", mock.Anything, uint64(3)).Return(nil).Once()
 	c.On("Peers").Return(uint64(4), nil).Once()
+	mw.On("Peers", mock.Anything, uint64(4)).Return(nil).Once()
 	c.On("Peers").Return(uint64(5), nil).Once()
+	mw.On("Peers", mock.Anything, uint64(5)).Return(nil).Once()
 	n.On("Info", "connected peers (5) >= 5").Once()
 	c.On("Peers").Return(uint64(5), nil).Once()
+	mw.On("Peers", mock.Anything, uint64(5)).Return(nil).Once()
 	c.On("Peers").Return(uint64(5), nil).Once()
+	mw.On("Peers", mock.Anything, uint64(5)).Return(nil).Once()
 	c.On("Peers").Return(uint64(5), nil).Once()
+	mw.On("Peers", mock.Anything, uint64(5)).Return(nil).Once()
 }
 
 func handleStatus(ctx context.Context, t *testing.T, n *mocks.Notifier) {
@@ -98,7 +106,7 @@ func TestMonitorHealth(t *testing.T) {
 	for _, chain := range chains {
 		handleIsBootstrappedChecks(t, notifier, client, chain)
 	}
-	handlePeers(notifier, client)
+	handlePeers(notifier, client, metricWriter)
 	handleIsHealthyChecks(notifier, client)
 	handleStatus(ctx, t, notifier)
 
